4fb0650cd1ac16ce94a90c874d4a27f5ac32053f
Klem26 <Kankina26@gmail.com>
2021-11-07 23:44:34 +0200

    create App + styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
+import React, { Suspense, lazy } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import AppBar from './components/AppBar/AppBar';
+import Container from './components/Container';
+import Loader from './components/Loader';
+import routes from './routes/routes';
 
+const LoginView = lazy(() =>
+  import('./views/Login/LoginView' /* webpackChunkName: "login-page" */),
+);
+const RequestView = lazy(() =>
+  import('./views/Request /RequestView' /* webpackChunkName: "request-page" */),
+);
+const RangeViews = lazy(() =>
+  import('./views/RangeView/RangeViews' /* webpackChunkName: "range-page" */),
+);
 
-function App() {
+const App = () => {
   return (
-    <div >
-      re
-    </div>
+    <Container>
+      <AppBar />
+      <Suspense fallback={<Loader/>}>
+        <Switch>
+          <Route exact path={routes.login} component={LoginView} />
+          <Route exact path={routes.users} component={RangeViews} />
+          <Route exact path={routes.request} component={RequestView} />
+        </Switch>
+      </Suspense>
+    </Container>
   );
-}
-
+};
 export default App;
