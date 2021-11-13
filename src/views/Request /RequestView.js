3c591c4587034359e0d7a12f60caac10b128987d
Klem26 <Kankina26@gmail.com>
2021-11-13 23:01:52 +0200

    update + react hooks

--- src/views/Request /RequestView.js
+++ src/views/Request /RequestView.js
@@ -1,37 +1,38 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
 import Filter from '../../components/Filter';
 import styles from './RequestView.module.css';
 
-class RequestView extends Component {
-  state = {
-    posts: [],
-  };
+const RequestView = () => {
+  const [posts, setPosts] = useState({});
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-  async componentDidMount() {
-    const response = await Axios.get(
+  const fetchData = async () => {
+    const result = await Axios(
       'https://jsonplaceholder.typicode.com/posts/?_limit=10',
     );
-    this.setState({ posts: response.data });
-  }
-  render() {
-    return (
-      <div className={styles.wrapper}>
-        <h1 className={styles.title}>Posts</h1>
-        <div className={styles.container}>
-          <Filter />
-          <ul className={styles.articles__list}>
-            {this.state.posts.map(post => (
+    setPosts(result.data);
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <h1 className={styles.title}>Posts</h1>
+      <div className={styles.container}>
+        <Filter />
+        <ul className={styles.articles__list}>
+          {posts.length > 0 &&
+            posts.map(post => (
               <li className={styles.articles__list_item} key={post.id}>
                 <h2 className={styles.title_h2}>{post.title}</h2>
                 <p className={styles.description}> {post.body}</p>
               </li>
             ))}
-          </ul>
-        </div>{' '}
-      </div>
-    );
-  }
-}
+        </ul>
+      </div>{' '}
+    </div>
+  );
+};
 
 export default RequestView;
